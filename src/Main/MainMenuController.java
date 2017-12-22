5001af6c857fa93cab8b7dbabc6d696db7cae083
jr <lkdfkldsf>
2017-12-22 11:50:55 +0100

    MainMenuController hinzugefügt

---
+++ src/Main/MainMenuController.java
@@ -0,0 +1,13 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+
+public class MainMenuController {
+    private Main app;
+
+    public void setApplication(Main app)
+    {
+        this.app = app;
+    }
+}
