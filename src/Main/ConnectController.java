6b70717d1027f530ac2eb4b26a38a0fd26afdea3
JoshuaBurger <burgerj@dhbw-loerrach.de>
2018-01-10 15:34:46 +0100

    - Info Label für Verbindungsprozess angebunden - Nur ein Mal pro Laufzeit Application erstellen

--- src/Main/ConnectController.java
+++ src/Main/ConnectController.java
@@ -1,9 +1,12 @@
 package Main;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import com.aldebaran.qi.Application;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 public class ConnectController {
 
@@ -11,9 +14,12 @@ public class ConnectController {
     private TextField txtIP;
     @FXML
     private TextField txtPort;
+    @FXML
+    private Label lblInfo;
 
     private Main mainClass;
     private Application app;
+    private String naoUrl;
 
     public void setMainClass(Main main)
     {
@@ -22,20 +28,40 @@ public class ConnectController {
     }
 
     public void executeConnect() {
-        String sUrl = "tcp://" + txtIP.getText() + ":" + txtPort.getText();
+        boolean bConnected = false;
+
+        lblInfo.setTextFill(Color.BLACK);
+        lblInfo.setText("");
+
+        if( this.app == null ){
+            naoUrl = "tcp://" + txtIP.getText() + ":" + txtPort.getText();
+            // Application erstellen
+            app = new Application(new String[]{}, naoUrl);
+        }
 
         // Verbindung aufbauen
-        System.out.println("try connecting to " + sUrl + " ...");
-        this.app = new Application(new String[]{""}, sUrl);
         try{
-            this.app.start();
-            // erfolgreich verbunden
-            mainClass.startMainMenu();
+            lblInfo.setText("Verbinde zu " + naoUrl + " ...");
+            System.out.println("Verbinde zu " + naoUrl + " ...");
+            app.start();
+            bConnected = true;
         }
         catch(Exception e){
             System.out.println("Verbindung konnte nicht hergestellt werden.");
-            this.app.stop();
-            this.app= null;
+            lblInfo.setTextFill(Color.RED);
+            lblInfo.setText("Verbindung konnte nicht hergestellt werden.");
+            app.stop();
+        }
+
+        if( bConnected == true){
+            // erfolgreich verbunden
+            System.out.println("Verbunden.");
+            try {
+                mainClass.startMainMenu();
+            }
+            catch(Exception e) {
+                System.out.println("Hauptmenue kann nicht geoeffnet werden");
+            }
         }
     }
 
